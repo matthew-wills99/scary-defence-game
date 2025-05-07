2980bb37321e25bfe7e771face3c1d635301219c
ChaseBatten <w0766981@myscc.ca>
2025-05-07 18:00:52 -0400

    Continue on Doors Pt.2

--- Assets/House1/Scripts/PlayerActions.cs
+++ Assets/House1/Scripts/PlayerActions.cs
@@ -14,5 +14,41 @@ public class PlayerActions : MonoBehaviour
     public void OnUse()
     {
         if (Physics.Raycast(Camera.position, Camera.forward, out RaycastHit hit, MaxUseDistance, UseLayers))
+        {
+            if (hit.collider.TryGetComponent<Door>(out Door door))
+            {
+                if (door.IsOpen)
+                {
+                    door.Close();
+                }
+                else
+                {
+                    door.Open(transform.position);
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Physics.Raycast(Camera.position, Camera.forward, out RaycastHit hit, MaxUseDistance, UseLayers)
+        && hit.collider.TryGetComponent<Door>(out Door door))
+        {
+            if (door.IsOpen)
+            {
+                UseText.SetText("Close \"E\"");
+            }
+            else
+            {
+                UseText.SetText("Open \"E\"");
+            }
+            UseText.gameObject.SetActive(true);
+            UseText.transform.position = hit.point - (hit.point - Camera.position).normalized * 0.01f;
+            UseText.transform.rotation = Quaternion.LookRotation((hit.point - Camera.postition).normalized);
+        }
+        else
+        {
+            UseText.gameObject.SetActive(false);
+        }
     }
 }
